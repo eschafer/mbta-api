a697d7e21a77803907a173b1b69555d9077f8739
Elizabeth Schafer <elizabeth.schafer.wenk@gmail.com>
2019-03-07 20:55:33 -0500

    added automatic linting+prettier on commit; reformatted files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
-import { ReactComponent as IconBlue } from "./img/icon-blue-line-small.svg";
-import { ReactComponent as IconGreenB } from "./img/icon-green-line-b-small.svg";
-import { ReactComponent as IconGreenC } from "./img/icon-green-line-c-small.svg";
-import { ReactComponent as IconGreenD } from "./img/icon-green-line-d-small.svg";
-import { ReactComponent as IconGreenE } from "./img/icon-green-line-e-small.svg";
-import { ReactComponent as IconMattapan } from "./img/icon-mattapan-line-small.svg";
-import { ReactComponent as IconOrange } from "./img/icon-orange-line-small.svg";
-import { ReactComponent as IconRed } from "./img/icon-red-line-small.svg";
-
-
+import { ReactComponent as IconBlue } from './img/icon-blue-line-small.svg';
+import { ReactComponent as IconGreenB } from './img/icon-green-line-b-small.svg';
+import { ReactComponent as IconGreenC } from './img/icon-green-line-c-small.svg';
+import { ReactComponent as IconGreenD } from './img/icon-green-line-d-small.svg';
+import { ReactComponent as IconGreenE } from './img/icon-green-line-e-small.svg';
+import { ReactComponent as IconMattapan } from './img/icon-mattapan-line-small.svg';
+import { ReactComponent as IconOrange } from './img/icon-orange-line-small.svg';
+import { ReactComponent as IconRed } from './img/icon-red-line-small.svg';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      subwayRoutes: []
+      subwayRoutes: [],
     };
 
     this.icons = {
@@ -27,18 +25,20 @@ class App extends Component {
       'Mattapan Trolley': <IconMattapan aria-hidden focusable="false" />,
       'Orange Line': <IconOrange aria-hidden focusable="false" />,
       'Red Line': <IconRed aria-hidden focusable="false" />,
-    }
+    };
   }
 
-  async getResource({ fields, filters, name, sort } = { fields: [], filters: [] }) {
-    const url = "https://api-v3.mbta.com";
+  async getResource(
+    { fields, filters, name, sort } = { fields: [], filters: [] }
+  ) {
+    const url = 'https://api-v3.mbta.com';
     const filtersQuery = filters
       .map(([key, value]) => `filter[${key}]=${value}`)
-      .join("&");
+      .join('&');
     const fieldsQuery = fields
       .map(([key, value]) => `fields[${key}]=${value}`)
-      .join("&");
-    const sortQuery = sort ? `sort=${sort}` : "";
+      .join('&');
+    const sortQuery = sort ? `sort=${sort}` : '';
 
     const response = await fetch(
       `${url}/${name}?${filtersQuery}&${fieldsQuery}&${sortQuery}`
@@ -54,9 +54,9 @@ class App extends Component {
     // reasons.
     const routes = await this.getResource({
       // subway includes "light rail" (type = 0) and "heavy rail" (type = 1)
-      filters: [["type", "0,1"]],
-      fields: [["route", "long_name"]],
-      name: "routes"
+      filters: [['type', '0,1']],
+      fields: [['route', 'long_name']],
+      name: 'routes',
     });
 
     // Get stops for each route
@@ -89,13 +89,13 @@ class App extends Component {
     // rendering it.
     for await (let route of routes.data) {
       const stops = await this.getResource({
-        filters: [["route", route.id]],
-        fields: [["stop", "name"]],
-        name: "stops"
+        filters: [['route', route.id]],
+        fields: [['stop', 'name']],
+        name: 'stops',
       });
       subwayRoutes.push({
         name: route.attributes.long_name,
-        stops: stops.data.map(node => node.attributes.name)
+        stops: stops.data.map((node) => node.attributes.name),
       });
 
       // sort routes by name
@@ -107,7 +107,7 @@ class App extends Component {
     }
 
     this.setState({
-      subwayRoutes
+      subwayRoutes,
     });
   }
 
@@ -127,10 +127,10 @@ class App extends Component {
       largestRoute = sortedSubwayRoutes[sortedSubwayRoutes.length - 1];
       smallestRoute = sortedSubwayRoutes[0];
     }
-    
+
     // get stops with more than one route
     const stopsWithConnections = [];
-    const stops = subwayRoutes.map(route => route.stops).flat();
+    const stops = subwayRoutes.map((route) => route.stops).flat();
     var countedStops = stops.reduce((allStops, stop) => {
       if (stop in allStops) {
         allStops[stop]++;
@@ -142,27 +142,27 @@ class App extends Component {
     Object.entries(countedStops).forEach(([key, value]) => {
       if (value > 1) stopsWithConnections.push(key);
     });
-    
+
     // get routes for all stops with connections
     // this should be part of the stuff above...
     const subwayStopsWithConnections = [];
     subwayRoutes.forEach((route) => {
       route.stops.forEach((stop) => {
-        if (stopsWithConnections.includes(stop)){
-          const stops = subwayStopsWithConnections.find((element) => element.name === stop);
+        if (stopsWithConnections.includes(stop)) {
+          const stops = subwayStopsWithConnections.find(
+            (element) => element.name === stop
+          );
           if (!stops) {
             subwayStopsWithConnections.push({
               name: stop,
-              routes: [route.name]
-            })
+              routes: [route.name],
+            });
           } else {
             stops.routes.push(route.name);
           }
         }
       });
     });
-
-
 
     return (
       <Fragment>
@@ -196,10 +196,10 @@ class App extends Component {
               <li>
                 <span>Stops with connections:</span>
                 <ul>
-                  {subwayStopsWithConnections.map(stop => (
+                  {subwayStopsWithConnections.map((stop) => (
                     <li key={stop.name}>
-                      {stop.name}{" "}
-                      {stop.routes.map(name => (
+                      {stop.name}{' '}
+                      {stop.routes.map((name) => (
                         <span className="icon" key={name}>
                           {this.icons[name]}
                         </span>
@@ -210,19 +210,13 @@ class App extends Component {
               </li>
             </ul>
             <h2>Trip Planner</h2>
-            <select
-              value={this.state.stopA}
-              onChange={this.handleStopAChange}
-            >
-              {stops.map(stop => (
+            <select value={this.state.stopA} onChange={this.handleStopAChange}>
+              {stops.map((stop) => (
                 <option value={stop}>{stop}</option>
               ))}
             </select>
-            <select
-              value={this.state.stopB}
-              onChange={this.handleStopBChange}
-            >
-              {stops.map(stop => (
+            <select value={this.state.stopB} onChange={this.handleStopBChange}>
+              {stops.map((stop) => (
                 <option value={stop}>{stop}</option>
               ))}
             </select>
